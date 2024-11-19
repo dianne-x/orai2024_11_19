1fa34daf0bf9e8ffdf484c81f934b0e2f4ce5a05
Bobák Kornél <143836544+dianne-x@users.noreply.github.com>
2024-11-19 09:28:02 +0100

    Add project files.

---
+++ Ber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orai2024_11_19
+{
+    internal class Ber
+    {
+        public string nev { get; set; }
+        public bool nem { get; set; }
+
+        public string reszleg { get; set; }
+
+        public int belepes { get; set; }
+        public int ber { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{nev} {(nem ? "férfi" : "nő")} {reszleg} {belepes} {ber}";
+        }
+        public Ber(string x)
+        {
+            string[] sor = x.Split(';');
+            nev = sor[0];
+            nem = sor[1] == "férfi";
+            reszleg = sor[2];
+            belepes = int.Parse(sor[3]);
+            ber = int.Parse(sor[4]);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using orai2024_11_19;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+var berek = new List<Ber>();
+using var sr = new StreamReader(@"..\..\..\src/berek2020.txt", Encoding.UTF8);
+_ = sr.ReadLine();
+
+while (!sr.EndOfStream)
+{
+    berek.Add(new Ber(sr.ReadLine()));
+}
+
+sr.Close();
+
+//f3
+Console.WriteLine($"{berek.Count} db számú dolgozó adata található meg a forrásban.");
+
+//f4
+var atlagBer = Math.Round(berek.Average(x => x.ber)/1000,1);
+Console.WriteLine($"A dolgozók átlagbére 2020-ban: {atlagBer} ezer forint");
+
+//f5 + f6
+Console.WriteLine($"Adjon meg egy részleget!");
+var reszlegInput = Console.ReadLine();
+var reszleg = berek.Where(x => x.reszleg == reszlegInput).ToList();
+if (reszleg.Count == 0)
+{
+    Console.WriteLine("A megadott részleg nem létezik a cégnél!");
+}
+else
+{
+    //legmagasabb fizetesu dolgozo adatai a reszlegen
+    var f6 = reszleg.OrderByDescending(x => x.ber).First();
+    Console.WriteLine($"{f6.nev} {(f6.nem ? "férfi" : "nő")} {f6.reszleg} {f6.belepes} {f6.ber}");
+}
+
+Console.WriteLine($"-----------------------------------------------------------");
+
+//f7
+var reszlegek = berek.Select(x => x.reszleg).Distinct().ToList();
+foreach (var r in reszlegek)
+{
+    var dolgozokSzama = berek.Count(x => x.reszleg == r);
+    Console.WriteLine($"{r} részlegen {dolgozokSzama} dolgozó dolgozik");
+}
+
+//kb 20 perc
+
+Console.ReadKey();
